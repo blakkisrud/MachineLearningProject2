Fixed backpropagation to work with sigmoid activation for hidden layers, identity for final layer

src/neural_net.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 import project_2_utils as utils
 import autograd.numpy as np
@@ -12,16 +13,21 @@ from matplotlib import pyplot as plt
 
 
 class fnn():
-    def __init__(self, dim_input, dim_output, dims_hiddens, activation_func, outcome_func, learning_rate=1e-4):
+    def __init__(self, dim_input, dim_output, dims_hiddens, activation_func, outcome_func, activation_func_deriv, outcome_func_deriv, learning_rate=1e-4):
         self.dim_input = dim_input
         self.dim_output = dim_output
 
         self.dims_hiddens = dims_hiddens    # tuple of neurons per hidden layer, e.g. (8) for single layer or (16, 8, 8) for three hidden layers
         self.num_hidden_layers = len(dims_hiddens)
         self.activation_func = activation_func
+        self.activation_func_deriv = activation_func_deriv
         self.outcome_func = outcome_func
+        self.outcome_func_deriv = outcome_func_deriv
+
 
         self.layer_sizes = [dim_input] + self.dims_hiddens + [output_dim]
+        self.num_layers = len(self.layer_sizes)
+
 
         self.weights = [np.random.randn(self.layer_sizes[i], self.layer_sizes[i+1]) for i in range(len(self.dims_hiddens) + 1)]
         self.biases = [np.random.randn(self.layer_sizes[i+1]) for i in range(len(self.dims_hiddens) + 1)]
@@ -41,8 +47,11 @@ class fnn():
         dims_prev = self.dim_input
 
         for dims in [*self.dims_hiddens, self.dim_output]:
-            weights.append(np.random.randn(dims_prev, dims))
-            biases.append(np.random.randn(dims))
+            # weights.append(np.random.randn(dims_prev, dims))
+            # biases.append(np.random.randn(dims))
+
+            weights.append(np.random.normal(size=(dims_prev, dims)))
+            biases.append(np.random.normal(size=(dims)))
 
             dims_prev = dims
 
@@ -55,42 +64,85 @@ class fnn():
     def predict_feed_forward(self, X, **kwargs):
         print(f"PREDICTING BY FEED-FORWARDING INPUT {X.shape} THROUGH NETWORK") if kwargs.get("verbose", 0) else 0
         self.activations = [X]
+        self.weighted_inputs = []
+
         i = 0
         Al = X
         for Wl, bl in zip(self.weights, self.biases):
             Zl = Al @ Wl + bl
             # todo: linear activation function for final layer? (i.e. the identity function)
             i += 1
-            if i > self.num_hidden_layers:
+
+            if i > self.num_hidden_layers:  # FINAL LAYER
                 Al = self.outcome_func(Zl)
-            else:
+
+            else:                           # HIDDEN LAYERS
                 Al = self.activation_func(Zl)
+
             self.activations.append(Al)
+            self.weighted_inputs.append(Zl)
 
         self.A = Al
         return Al
 
 
     def backpropagate(self, y, **kwargs):
-        print("activations", [np.shape(a) for a in self.activations])
-        print("weights", [np.shape(w) for w in self.weights])
+        # print("activations", [np.shape(a) for a in self.activations])
+        # print("weights", [np.shape(w) for w in self.weights])
+        verbose = kwargs.get("verbose", True)
+
+        # TODO: GENERALIZE TO ARBITRARY ACTIVATION FUNCTIONS, WITH DERIVATIVES
+
+        # TODO: change to "arbitrary" cost-derivative function
+        num_obs = len(y)
+
+        loss = np.square(self.activations[-1] - y)     # prediction - ground truth squared
+
+        dC = 2 * (self.activations[-1] - y)     # derivative of squared error
+
+        if verbose:
+            print("num layers\t", self.num_layers)
+            print("weights\t", [np.shape(w) for w in self.weights])
+            print("biases\t", [np.shape(b) for b in self.biases])
+            print("activations\t\t", [np.shape(a) for a in self.activations])
+            print("weighted inputs\t", [np.shape(z) for z in self.weighted_inputs])
 
 
-        loss = np.square(self.activations[-1] - y)     # prediction - ground truth
+        for l in range(self.num_layers - 2, -1, -1):
+            print("l=", l) if verbose else 0
 
-        dA = 2 * (self.activations[-1] - y)     # derivative of squared error
-        # dZ = dA * self.sigmoid_derivatives(self.A)
-        self.sigmoid_derivatives = []
-        for i in range(len(self.weights)-1, -1, -1):
-            sd = utils.sigmoid_derivated(self.A)
-            self.sigmoid_derivatives.append(sd)
-            dZ = dA * utils.sigmoid_derivated(self.activations[i+1])
-            dW = np.dot(self.activations[i].T, dZ)
-            db = np.sum(dZ, axis=0)
-            self.weights[i] -= self.learning_rate * dW
-            self.biases[i] -= self.learning_rate * db
-            dA = np.dot(dZ, self.weights[i].T)
+            # FINAL / OUTPUT LAYER
+            if l == self.num_layers - 2:
+                # print("FINAL LAYER")
+                f_deriv_zl = self.outcome_func_deriv(self.weighted_inputs[l])
 
+
+            # HIDDEN LAYER
+            else:
+                # print("HIDDEN LAYER")
+                f_deriv_zl = self.activation_func_deriv(self.weighted_inputs[l])
+
+            # print("dC, fderiv_l", dC.shape, f_deriv_zl.shape)
+            delta_l = dC * f_deriv_zl
+            # print("delta_l", delta_l.shape)
+            # print("activations_l.T, delta_l", self.activations[l].T.shape, delta_l.shape)
+
+
+            # cost rate of change with respect to weights and biases in layer l
+
+            dW = np.dot(self.activations[l].T, delta_l)
+            db = np.sum(delta_l, axis=0)
+
+            # print("dW, db", dW.shape, db.shape)
+            # print(dW)
+
+
+            self.weights[l] -= self.learning_rate * dW / num_obs
+            self.biases[l] -= self.learning_rate * db / num_obs
+
+            dC = np.dot(delta_l, self.weights[l].T)
+
+        # sys.exit()
         return loss
 
 
@@ -104,25 +156,36 @@ class fnn():
             plot = True
 
         # fig, ax = plt.subplots() if plot else (0, 0)
-        errs = []
+        loss_for_epochs = []
+
         for e in range(epochs):
+
             if stochastic:
                 from sklearn.utils import resample
                 X_samp, y_samp = resample(X, y)
                 self.predict_feed_forward(X_samp)
-                loss = self.backpropagate(y_samp)
+                loss = self.backpropagate(y_samp, **kwargs)
+                # print("STOCHASTIC")
             else:
+                # print("NON_STOCHASTIC")
                 self.predict_feed_forward(X)
-                loss = self.backpropagate(y)
+                loss = self.backpropagate(y, **kwargs)
             # ax.plot(list(range(len(loss))), loss, "x", label=f"{e}") if plot else 0
-            errs.append(np.mean(loss))
-            print(loss.shape) if kwargs.get("verbose", 0) else 0
-        ax.plot(list(range(epochs)), errs) if plot else 0
+            # print(e, loss.shape, f"{np.mean(loss):.3e}, {np.median(loss):.3e}")
+            print(e, loss.shape, f"{np.mean(loss):.3e}, {np.median(loss):.3e}", self.weights, self.biases) if kwargs.get("verbose", 0) else 0
+
+            loss_for_epochs.append(np.mean(loss))
+
+            # print(loss.shape) if kwargs.get("verbose", 0) else 0
+            # print(e, f"{np.mean(loss):.3e}")
+
+
+        ax.plot(list(range(epochs)), loss_for_epochs) if plot else 0
         ax.set_title(kwargs.get("plot_title")) if plot else 0
         ax.set_xlabel(kwargs.get("plot_xlabel")) if plot else 0
 
         plt.show() if showplot else 0
-        return errs
+        return loss_for_epochs
 
 
 if __name__ == "__main__":
@@ -140,9 +203,10 @@ if __name__ == "__main__":
         if not input_mode in valid_inputs:
             print("Invalid input, please try again...")
 
+    # LOAD ONE-DIM FUNCTION R1 -> R1
     if input_mode == 1:
         print("LOADING SIMPLE ONE-DIMENSIONAL FUNCTION")
-        x = np.arange(0, 10, 0.1)
+        x = np.arange(0, 10, 0.01)
         X = utils.one_d_design_matrix(x, n=1)
         X = X[:, 1]     # remove bias from design matrix
         X = X.reshape(-1, 1)
@@ -154,7 +218,7 @@ if __name__ == "__main__":
         # plt.plot(x, y)
         # plt.show()
 
-
+    # LOAD SINGLE MNIST-IMAGE R2 -> R2
     if input_mode == 2:
         print("LOADING SINGLE MNIST IMAGE")
         dataset_digits = load_digits()
@@ -172,37 +236,62 @@ if __name__ == "__main__":
 
 
     y = MinMaxScaler(feature_range=(0, 1)).fit_transform(y.reshape(-1, 1)).reshape(-1, 1)
+    num_obs = len(y)
     print("SHAPE x / y:", X.shape, y.shape)
 
 
-    activation_func = np.vectorize(lambda x: 1 / (1 + np.exp(-x)))  # sigmoidal activation
-    outcome_func = np.vectorize(lambda x: x)    # identity function
+    # activation_func = np.vectorize(lambda z: 1 / (1 + np.exp(-z)))  # sigmoidal activation
+    # outcome_func = np.vectorize(lambda z: z)    # identity function
+
+    activation_func = utils.sigmoid
+    activation_func_deriv = utils.sigmoid_derivated
+
+    outcome_func = utils.identity
+    outcome_func_deriv = utils.identity_derived
+
 
     # dims_hidden = [8, 8, 8]
-    dims_hidden = [4]
-    net = fnn(dim_input=input_dim, dim_output=output_dim, dims_hiddens=dims_hidden, activation_func=activation_func, outcome_func=outcome_func)
+    dims_hidden = [1]
+    # dims_hidden = []
+
+    lr = 1e-2
+    epochs = 100
+
+    stochastic = True
+
+
+    net = fnn(dim_input=input_dim, dim_output=output_dim, dims_hiddens=dims_hidden, learning_rate=lr,
+              activation_func=activation_func, outcome_func=outcome_func, activation_func_deriv=activation_func_deriv, outcome_func_deriv=outcome_func_deriv)
 
     # net.predict_feed_forward(X)
     # net.backpropagate(y)
 
 
     # Plot MSE for epochs for repeated random initialization
+
+    nrand = 8
     plot = True
-    stochastic = True
+
+    fig_folder = r"figs\neural_test"
+    if not os.path.exists(fig_folder):
+        os.mkdir(fig_folder)
+
+    title = f"hidden dims = {net.dims_hiddens}, repeated with random initiation {nrand} times, eta={net.learning_rate:.3e}, stochastic={stochastic}, N_obs={num_obs}"
+    savename = f"nn={net.layer_sizes}_stochastic={stochastic}_lr={net.learning_rate}.png"
+    fig_path = os.path.join(fig_folder, savename)
+
+    from sklearn.metrics import mean_squared_error
 
     if plot:
-        fig, ax = plt.subplots(ncols=2)
+        fig, ax = plt.subplots(ncols=2, figsize=(12, 8))
         ax, ax1 = ax
-        ax.set_ylim(-1, 1)
         ax1.set_ylim(0, 1)
         # fig1, ax1 = plt.subplots()
 
-        lr = 5e-3
-        epochs = 10
         net.learning_rate = lr
         linewidth = 4.0
+        max_loss = 0
 
-        nrand = 50
         for i in range(nrand):
             net.init_random_weights_biases()
 
@@ -212,23 +301,37 @@ if __name__ == "__main__":
                 # errs = net.train(X_samp, y_samp, plot=False, figax=(fig, ax), showplot=False, plot_title=f"MSE lr = {net.learning_rate}")
 
             # else:
-            errs = net.train(X, y, stochastic=True, plot=False, figax=(fig, ax), showplot=False, plot_title=f"MSE lr = {net.learning_rate}")
+            loss_epochs = net.train(X, y, epochs=epochs, stochastic=stochastic, plot=False, figax=(fig, ax), showplot=False, plot_title=f"MSE lr = {net.learning_rate}", verbose=False)
+            # print(net.weights, net.biases)
 
-            ax.plot(list(range(len(errs))), errs)
+            loss_final = loss_epochs[-1]
+            max_loss = loss_final if loss_final > max_loss else max_loss
 
             yhat = net.predict_feed_forward(X)
-            ax1.plot(x, y, linewidth=linewidth, c="black")
-            ax1.plot(x, yhat, ":")
+            mse = mean_squared_error(y, yhat)
+            mse2 = mean_squared_error(y, net.activations[-1])
+            print(i, f"mse={mse:.2e}, {mse2:.2e}")
+            print(f"weights", net.weights, "biases", net.biases)
 
-        ax.plot([0, epochs], [0, 0], linewidth=linewidth, c="black")
-        ax.set_title(f"MSE post-training with random initiation {nrand} times for eta={net.learning_rate}")
+
+            ax.plot(list(range(len(loss_epochs))), loss_epochs, c=f"C{i}", label=i)
+            ax1.plot(x, yhat, c=f"C{i}", label=i)
+
+
+        ax1.plot(x, y, linewidth=linewidth, c="black", zorder=0)
+        # ax.plot([0, epochs], [0, 0], linewidth=linewidth, c="black", zorder=0)
+        ax.set_title(f"MSE during training")
         ax.set_xlabel("Epochs")
 
-        ax1.set_title(f"predictions post-training with random initiation {nrand} times for eta={net.learning_rate}")
+        ax1.set_title(f"predictions post-training")
         ax1.set_xlabel("x")
+        ax.set_ylim(0, max_loss*1.1)
 
-        fig.suptitle(f"hidden dims = {net.dims_hiddens}")
+        ax.legend()
+        ax1.legend()
 
+        fig.suptitle(title)
+        fig.savefig(fig_path)
         plt.show()
 
 

src/project_2_utils.py
@@ -27,7 +27,7 @@ class Scheduler():
 
     def update_change(self, gradient):
         raise NotImplementedError
-        
+
     def reset(self):
         pass
 
@@ -38,7 +38,7 @@ class ConstantScheduler(Scheduler):
 
     def update_change(self, gradient):
         return self.eta*gradient
-    
+
     def reset(self):
         pass
 
@@ -131,9 +131,9 @@ def general_gradient_descent(X, y, beta, scheduler,
                              epsilon = 1.0e-4,
                              max_iterations = 100000,
                              return_diagnostics = False):
-    
+
     """
-    This is the general gradient descent, can be performed with 
+    This is the general gradient descent, can be performed with
     all cost functions and gradients (that need to be analytically defined)
     """
 
@@ -170,11 +170,11 @@ def general_gradient_descent(X, y, beta, scheduler,
                              "beta": beta}
 
         return diagnostic_output
-    
+
     else:
 
         return beta
-    
+
 def general_stochastic_gradient_descent(X, y, beta, scheduler,
                                         cost_func,
                                         mini_batch_size,
@@ -185,7 +185,7 @@ def general_stochastic_gradient_descent(X, y, beta, scheduler,
                                         return_diagnostics = False):
 
     """
-    This is the general stochastic gradient descent, can be performed with 
+    This is the general stochastic gradient descent, can be performed with
     all cost functions and gradients (that need to be analytically defined)
     """
 
@@ -223,7 +223,7 @@ def general_stochastic_gradient_descent(X, y, beta, scheduler,
                              "beta": beta}
 
         return diagnostic_output
-    
+
     else:
 
         return beta
@@ -397,6 +397,32 @@ def gradient_descent_with_minibatches(X, y, beta, eta, minibatch_size = 5, VERBO
 
     return beta, scores
 
+
+def sigmoid(z):
+    f = lambda z: 1 / (1 + np.exp(-z))
+    vf = np.vectorize(f)
+    return vf(z)
+
+
+def sigmoid_derivated(z):
+    f = lambda z: sigmoid(z) * (1 - sigmoid(z))
+    vf = np.vectorize(f)
+    return vf(z)
+
+
+def identity(z):
+    f = lambda z: z
+    vf = np.vectorize(f)
+    return vf(z)
+
+
+def identity_derived(z):
+    f = lambda z: 1
+    vf = np.vectorize(f)
+    return vf(z)
+
+
+
 def gradient_descent_with_time_decay(X, y, beta, eta0, minibatch_size=5):
 
     n_epochs = 50
@@ -433,9 +459,9 @@ def gradient_descent_with_time_decay(X, y, beta, eta0, minibatch_size=5):
 
     return beta, scores
 
-        
 
-    
+
+
 
     return 0
 